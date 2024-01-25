3fc19998d003cb6c1f2c1ee5e5792a0610d7119e
Andronachi Andrei <andrei.andronachi95@gmail.com>
2024-01-25 15:08:29 +0200

    update pdf

--- src/app/FileComponent.tsx
+++ src/app/FileComponent.tsx
@@ -12,11 +12,21 @@ function FileComponent() {
   }, [])
     const data = {
         name: "Andrei",
-        age: 29
+        age: 29,
+        data: "2024-01-27",
     }
+    const company = {
+      id: 1,
+      name: "Company",
+    }
+    const status = ['1', '2,', '3'];
+    const people = [
+      {id: 1, name: "Andrei"},
+      {id: 2, name: "Test"}
+    ]
   return (
     <>
-    {a && <PDFDownloadLink fileName='download.pdf' document={<FilePdf data={data} />}>{({ blob, url, loading, error }) =>
+    {a && <PDFDownloadLink fileName='download.pdf' document={<FilePdf data={data} company={company} status={status} people={people} />}>{({ blob, url, loading, error }) =>
         loading ? 'Loading...' : 'Download now!'
       }</PDFDownloadLink>}
         
--- src/app/FilePdf.tsx
+++ src/app/FilePdf.tsx
@@ -6,10 +6,30 @@ interface FilePdfProps {
     data: {
       name: string;
       age: number;
+      data: string;
     };
+    company: {
+        id: number;
+        name: string;
+    };
+    status: string[];
+    people: {
+        id: number;
+        name: string;
+    }[]
   }
 
-const FilePdf: React.FC<FilePdfProps> = ({data}) => {
+const FilePdf: React.FC<FilePdfProps> = ({data, company, status, people}) => {
+    const originalDate = new Date(data.data);
+  originalDate.setDate(originalDate.getDate() - 1);
+  const signDate = originalDate.toISOString().split('T')[0];
+  const employee = people.find(el => el.name === data.name)
+
+
+
+
+
+
     return (
         <Document>
             <Page size="A4">
@@ -18,6 +38,17 @@ const FilePdf: React.FC<FilePdfProps> = ({data}) => {
                 </View>
                 <View>
                     <Text>{data.age}</Text>
+                </View>
+                <View><Text>{company.name}</Text></View>
+                <View><Text>{signDate}</Text></View>
+                <View><Text>{employee?.name}</Text></View>
+                <View >
+                    {status.map((el, index) => (
+                        <Text key={index}
+                        >
+                        {el}
+                        </Text>
+                    ))}
                 </View>
             </Page>
         </Document>

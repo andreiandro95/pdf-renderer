82f62a62525424b81041c50d77bd8712a7fe1c4d
Andronachi Andrei <andrei.andronachi95@gmail.com>
2024-02-15 12:00:20 +0200

    email

--- src/app/api/cron/test/route.tsx
+++ src/app/api/cron/test/route.tsx
@@ -8,14 +8,18 @@ export async function GET (request: NextRequest){
         const array = [1,2,3]
         const arrayEmail= ['aa@armuro.com', 'andrei.andronachi95@gmail.com']
         const emailList = arrayEmail.toString()
-  
+        array.forEach((id) => {
+            arrayEmail.forEach(async (email) => {
                 const isSent = await resendSendMail({
-                    to: 'andrei.andronachi95@gmail.com',
+                    to: email,
                     subject: `Test`,
-                    component: `<p>${'testss'} - a</p>`,
+                    component: `<p>${id} - a</p>`,
                   });
                   if (!isSent) throw new Error('Something went wrong');
-
+            })
+            
+              
+        })
         return NextResponse.json({message: "OK"}, {status: 200})
     }catch (error) {
         return NextResponse.json({error: error}, {status: 500})
